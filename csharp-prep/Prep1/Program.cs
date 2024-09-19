7dbc61aedf9703133dd7097ec84cb8cf90505268
jalufi <168149096+zzjhkj@users.noreply.github.com>
2024-09-18 20:21:44 -0600

    delete space

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,6 +8,6 @@ class Program
         string first_name = Console.ReadLine();
         Console.WriteLine("What is your last name?");
         string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name}  {last_name}");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
