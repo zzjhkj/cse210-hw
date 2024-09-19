2e06f65939286e57d72ed500a7d0d4fe12eee20c
jalufi <168149096+zzjhkj@users.noreply.github.com>
2024-09-18 20:18:12 -0600

    add string

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name}  {last_name}");
     }
 }
