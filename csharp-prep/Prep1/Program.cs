16c4f91cbfb4673d3208591d715d63e5f7a34602
jalufi <168149096+zzjhkj@users.noreply.github.com>
2024-09-19 18:36:21 -0600

    change the variable name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.WriteLine("What is your last name?");
-        string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
